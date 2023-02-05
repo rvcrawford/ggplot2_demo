d91c0e2116363299e37766511b4ed1cd351071b2
rvcrawford <rvcrawford@gmail.com>
2023-02-05 09:41:20 -0500

    add basics scripts
    basic ggplot manipulations

---
+++ 2_toggling_options.R
@@ -0,0 +1,78 @@
+#### 2. SOME OPTIONS #######
+
+# adjust theme
+iris_plot + 
+  theme_bw()
+
+# adjust theme + size
+
+iris_plot + 
+  theme_bw(24)
+
+# adjust legend position
+
+# (helpful page for this)
+# http://www.sthda.com/english/wiki/ggplot2-legend-easy-steps-to-change-the-position-and-the-appearance-of-a-graph-legend-in-r-software
+
+iris_plot + 
+  theme(legend.position="bottom")
+
+# put legend in the plot
+iris_plot +
+  theme(legend.position = c(0.9, 0.8))
+
+# turn off legend position
+iris_plot + 
+  theme(legend.position='none')
+
+# change fonts
+iris_plot + 
+  theme(text=element_text(family="serif"))
+
+# change fonts again
+iris_plot + 
+  theme(text=element_text(family="Palantino"))
+
+# my impression is that there may be differences in this stuff depending on your OS.
+
+##### adding lines/ smoothers
+
+# add loess smoother
+cars_plot + 
+  geom_smooth()
+
+# add a linear model
+cars_plot + 
+  geom_smooth(method = "lm")
+
+# smooth by groups
+iris_plot + 
+  geom_smooth()
+
+# what if we just want one line, but colored dots
+iris_plot + 
+  geom_smooth(aes(x = Sepal.Width, y = Sepal.Length))
+
+
+# modfiying a bunch of parameters 
+
+# ggplot docs contain a bunch of info on what is what
+iris |> 
+  ggplot(aes(x = Sepal.Width, y = Sepal.Length)) + 
+  # modifying shapes
+  geom_point(aes(col = Species), shape = 4) + 
+  # turned off standard errors
+  # modified line type argument
+  # made it big
+  # note that this is kind of silly
+  geom_smooth(method = "lm", se = F, lty = 4, color = "darkblue", size = 3)
+
+
+# one more for good measure...
+
+
+# using shape to distinguish between species...
+iris |> 
+  ggplot(aes(x = Sepal.Width, y = Sepal.Length)) + 
+  # modifying shapes
+  geom_point(aes(shape = Species))
---
+++ scripts/1_basics.R
@@ -0,0 +1,55 @@
+#### 1. BASICS #######
+
+library(ggplot2)
+
+# load datasets
+data(mtcars)
+data(iris)
+
+# in base R
+plot(mtcars$wt, mtcars$hp)
+
+####### ggplot 2 ########3
+
+# - note use of base-R pipe
+
+# note that nothing is plotted yet - we need a geom!
+mtcars |> 
+  ggplot(aes(wt, hp))
+
+cars_plot <- mtcars |> 
+  ggplot(aes(wt, hp)) + 
+  geom_point()
+
+#Q: Why doesn't the following work? 
+
+mtcars %>%
+  ggplot(aes(wt, hp)) + 
+  geom_point()
+
+# A: "%>%" comes from maggrittr, which we did not load (would have loaded with library(tidyverse),
+# if we did this, we would also need to use a "." before "aes" in the ggplot() call)
+
+
+# looking at iris, make plots colored by species 
+
+(iris_plot <- iris |> 
+  ggplot(aes(x = Sepal.Width, y = Sepal.Length, col = Species)) + 
+  geom_point())
+
+# how about a boxplot?
+
+(iris_boxplot <- iris |> 
+    ggplot(aes(x = Species, y = Petal.Length)) + 
+    geom_boxplot())
+
+(iris_hist <- iris |> 
+  ggplot(aes( x = Petal.Length, col = Species)) + 
+  geom_histogram())
+
+# histogram by groups
+(iris_hist <- iris |> 
+    # note only x supplied, used "fill" argument to specify filling the histogram
+    ggplot(aes( x = Petal.Length, fill = Species)) + 
+    # adjusted
+    geom_histogram(bins = 20))
